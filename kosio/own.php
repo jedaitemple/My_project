21aa9fd039b03a415fe8fba7c4f7b6e22913ca60
jedaitemple <12122004r>
2015-07-09 18:26:26 +0300

    add
    add

--- kosio/own.php
+++ kosio/own.php
@@ -77,99 +77,20 @@ body {
 if(isset($_POST['submit'])){
 $selected_val = $_POST['topic']; 
 echo "You have selected :" .$selected_val; 
-		$link1 = $_POST['link'];
-		$link1 = mysql_real_escape_string($_POST['link']);
-
-		$image =' ';
 		$link = mysqli_connect("localhost", "root", "", "kosio");
-		
+		$image = $_POST['image'];
+		$image = mysql_real_escape_string($_POST['image']);
+			$head = $_POST['head'];
+		$head = mysql_real_escape_string($_POST['head']);
 		$topic=$selected_val;
 		
-		
- 
-// Check connection
-if($link === false){
-    die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-
-
-
-
-			$dblink=$link1;
-			 $ch = curl_init($dblink);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.116 Safari/537.36');
-    $res = curl_exec($ch);
-    if ($res === false) {
-        die('error: ' . curl_error($ch));
-    }
-    curl_close($ch);
-
-
-/*** discard white space ***/
-
-
-    $d = new DOMDocument();
-    @$d->loadHTML($res);
-	 $d->preserveWhiteSpace = false;
-$images = $d->getElementsByTagName('img');
-
-
-
-    $output = array(
-      '',
-    );
-	 $output1 = array(
-      '',
-    );
-    $x = new DOMXPath($d);
-    $title = $x->query("//title");
-	$flag=0;
-    if ($title->length > 0) {
-        $output['title'] = $title->item(0)->textContent;
-    }
-	
-	$str = implode(',', $output);
-	$image = '';
-
-	require_once('additions/simple_html_dom.php');
-	require_once('additions/url_to_absolute.php'); // get image absolute url.
-// options
-$biggestImage = 'path to "no image found" image'; // Is returned when no images are found.
-// process
-$maxSize = -1;
-$visited = array();
-$url=$link1;
-$html = file_get_html($url);
-// loop images
-foreach($html->find('img') as $element) {
-    $src = $element->src;
-    if($src=='')continue;// it happens on your test url
-    $imageurl = url_to_absolute($url, $src);//get image absolute url
-    // ignore already seen images, add new images
-    if(in_array($imageurl, $visited))continue;
-    $visited[]=$imageurl;
-    // get image
-    $image=@getimagesize($imageurl);// get the rest images width and height
-    if (($image[0] * $image[1]) > $maxSize) {   
-        $maxSize = $image[0] * $image[1];  //compare sizes
-        $biggest_img = $imageurl;
-    }
-}
-$image=$biggest_img; //return the biggest found image
-
-
-
-// process
-
 $tz = 'Europe/Sofia';
 $timestamp = time();
 $dt = new DateTime("now", new DateTimeZone($tz)); 
 $dt->setTimestamp($timestamp); 
 $date=$dt->format('d.m.Y, H:i:s');
-	$sql = "INSERT INTO links (number, link, username,topic,links,image,date) VALUES (0, '$str', '$usname','$topic','$link1','$image','$date')";
-	print_r($output);
+	$sql = "INSERT INTO links (number, link, username,topic,links,image,date) VALUES (0, '$head', '$usname','$topic','','$image','$date')";
+	
 	
 	
 if(mysqli_query($link, $sql)){
@@ -203,9 +124,9 @@ body {
 		
 	
 		<div id="form">
-			<form align = "center" action = "addnews.php" method = "POST">
+			<form align = "center" action = "own.php" method = "POST">
 				<h2>select link of publication</h2>
-				<input placeholder = "link" style = "margin-top:5px;border: 1px solid black;width:317px;height:40px;" type = "text" name = "link" required/><br>
+				<input placeholder = "head" style = "margin-top:5px;border: 1px solid black;width:317px;height:40px;" type = "text" name = "head" required/><br>
 				<h2>select type of the news</h2>
 				<select name="topic" style = "margin-top:5px;border: 1px solid black;width:317px;height:40px;">
 <option     value="business">business</option>
@@ -218,7 +139,7 @@ body {
 <option value="comics">comics</option>
 
 </select>
-
+			<input placeholder = "image" style = "margin-top:5px;border: 1px solid black;width:317px;height:40px;" type = "text" name = "image" /><br>
 				<input class = "button" type = "submit" value = "ADD the news" name = "submit" />
 			</form>	
 		</div>
