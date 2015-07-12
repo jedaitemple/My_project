c20db398942a06d1dcaca5697c34d16b10f35095
jedaitemple <12122004r>
2015-07-12 22:24:05 +0300

    add email if it is valid
    add email if it is valid

--- kosio/register.php
+++ kosio/register.php
@@ -1,5 +1,32 @@
 <?php
 include_once("dbConnect.php");
+function check_email_address($email) {
+    // First, we check that there's one @ symbol, and that the lengths are right
+    if (!ereg("^[^@]{1,64}@[^@]{1,255}$", $email)) {
+        // Email invalid because wrong number of characters in one section, or wrong number of @ symbols.
+        return false;
+    }
+    // Split it into sections to make life easier
+    $email_array = explode("@", $email);
+    $local_array = explode(".", $email_array[0]);
+    for ($i = 0; $i < sizeof($local_array); $i++) {
+         if (!ereg("^(([A-Za-z0-9!#$%&'*+/=?^_`{|}~-][A-Za-z0-9!#$%&'*+/=?^_`{|}~\.-]{0,63})|(\"[^(\\|\")]{0,62}\"))$", $local_array[$i])) {
+            return false;
+        }
+    }    
+    if (!ereg("^\[?[0-9\.]+\]?$", $email_array[1])) { // Check if domain is IP. If not, it should be valid domain name
+        $domain_array = explode(".", $email_array[1]);
+        if (sizeof($domain_array) < 2) {
+                return false; // Not enough parts to domain
+        }
+        for ($i = 0; $i < sizeof($domain_array); $i++) {
+            if (!ereg("^(([A-Za-z0-9][A-Za-z0-9-]{0,61}[A-Za-z0-9])|([A-Za-z0-9]+))$", $domain_array[$i])) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 	
 if(isset($_POST['submit'])){
 		
@@ -18,6 +45,12 @@ if(isset($_POST['submit'])){
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
+if (check_email_address($email)) {
+    echo $email . ' is a valid email address.';
+} else {
+    echo $email . ' is not a valid email address.';
+}
+
 			
 			$sql = "INSERT INTO kosio1 (id, username, password, email,activated) VALUES (0, '$username', '$pass1', '$email','1')";
 if(mysqli_query($link, $sql)){
@@ -32,7 +65,7 @@ if(mysqli_query($link, $sql)){
 			exit();
 		}
 		
-		header('Location:index.php');
+		//header('Location:index.php');
 		
 } else {
 	
